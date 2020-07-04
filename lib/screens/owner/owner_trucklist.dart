b07ea124aac9c605c01388ffe31222468f544f31
ounamg <ounamg008@gmail.com>
2020-07-05 00:26:21 +0530

    Commit 13 - Data Forwarding

--- lib/screens/owner/owner_trucklist.dart
+++ lib/screens/owner/owner_trucklist.dart
@@ -20,7 +20,9 @@ class _OwnerTruckListState extends State<OwnerTruckList> {
   List<String> managerName =[];
   List<int> managerLid =[];
   var resultFinal;
-  var res1;
+  int res1;
+  bool _isLoading = true;
+
 
   Future setTruckDetails() async {
 
@@ -30,93 +32,107 @@ class _OwnerTruckListState extends State<OwnerTruckList> {
         user: 'root',
         db: 'logistic',
         password: 'Ati@radeon1'));
-    var result1 = await conn.query("select count(*) from trucks group by owner_lid=$cid");
+    var result1 = await conn.query("select count(*) from trucks where owner_lid=$cid");
     var result2 = await conn.query("select truck_number, vehicle_class,driver_name,driver_lid,manager_name,manager_lid from trucks where owner_lid=$cid");
-    res1= result1;
     resultFinal= result2;
-//    for(var row in result2) {
-//      truckNum.add(row[0]);
-//      vehicleClass.add(row[1]);
-//      driverName.add(row[2]);
-//      driverLid.add(row[3]);
-//      managerName.add(row[4]);
-//      managerLid.add(row[5]);
-//    }
+    for(var ro in result1){
+      res1= ro[0];
+    }
+    for(var row in result2) {
+      truckNum.add(row[0]);
+      vehicleClass.add(row[1]);
+      driverName.add(row[2]);
+      driverLid.add(row[3]);
+      managerName.add(row[4]);
+      managerLid.add(row[5]);
+      setState(() {
+        _isLoading = false;
+      });
+
+    }
+//    print(res1);
 //    print(truckNum);
 //    print(managerLid);
     await conn.close();
+
+//    print(truckNum);
   }
 
 
-  int value = 2;
 
-  _addItem() {
-    setState(() {
-      value = value + 1;
-    });
+
+
+  @override
+  void initState() {
+    super.initState();
+    setTruckDetails();
   }
 
 
   @override
   Widget build(BuildContext context) {
-    setTruckDetails();
-    return new Scaffold(
+
+    return Scaffold(
       appBar: AppBar(
         title: Text('Trucks'),
       ),
-      body: ListView.builder(
-          itemCount: this.value,
-          itemBuilder: (context, index) => this._buildRow(index)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
-        child: Icon(Icons.add),
-      ),
+      body: Container(
+      height: MediaQuery.of(context).size.height,
+    decoration: BoxDecoration(
+    color: Colors.white,
+    ),
+    child: _isLoading
+    ? Center(
+    child: CircularProgressIndicator(),
+    )
+        :ListView.builder(
+          itemCount: res1,
+          itemBuilder: (context, index) => _buildRow(index)),
+    )
     );
   }
 
-
-
   _buildRow(int index) {
-      return Card(
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(3.0),
-                child: Wrap(
-                  alignment: WrapAlignment.start,
-                  children: <Widget>[
-                    Text('${index.toString()}  '),
-                    Text('Truck No. :- ${truckNum[index]}'),
-//                Spacer(flex: 1,),
-//                    Text('Driver Id :- ${driverLid[index]}'),
-                    Text('Driver Name :- ${driverName[index]}'),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Wrap(
-//              alignment: WrapAlignment.start,
-//              crossAxisAlignment: WrapCrossAlignment.start,
-                  children: <Widget>[
-                    Text('Vehicle Class :- ${vehicleClass[index]}'),
-                    Text('Manager Name:- ${managerName[index]}'),
-                    Text('Manager Id:- ${managerLid[index]}'),
-                  ],
-                ),
-              ),
-              Wrap(
+    return Card(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Wrap(
                 alignment: WrapAlignment.start,
                 children: <Widget>[
-                  Text('City:- ')
+                  Text('${index.toString()}  '),
+                  Text('Truck No. :- ${truckNum[index]}'),
+//                Spacer(flex: 1,),
+//                    Text('Driver Id :- ${driverLid[index]}'),
+                  Text('Driver Name :- ${driverName[index]}'),
                 ],
-              )
-            ],
-          )
-      );
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Wrap(
+//              alignment: WrapAlignment.start,
+//              crossAxisAlignment: WrapCrossAlignment.start,
+                children: <Widget>[
+                  Text('Vehicle Class :- ${vehicleClass[index]}'),
+                  Text('Manager Name:- ${managerName[index]}'),
+                  Text('Manager Id:- ${managerLid[index]}'),
+                ],
+              ),
+            ),
+            Wrap(
+              alignment: WrapAlignment.start,
+              children: <Widget>[
+                Text('City:- ')
+              ],
+            )
+          ],
+        )
+    );
 
   }
 }
